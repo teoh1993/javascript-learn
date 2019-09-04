29fbbd9668eb751aaf6d18c2c55b543779f53583
Neo <teoh1993@hotmail.com>
2019-09-04 21:44:23 +0800

    Update index.js

--- tutorial/index.js
+++ tutorial/index.js
@@ -1,18 +1,18 @@
 
 
-console.log('hello world');
+// console.log('hello world');
 
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('HHHello World!');
 
+//comment
 
-console.log('hello world');
+	console.log('wakakakak');
 
-
-
+	// var
 
 
 
