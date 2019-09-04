515c39fe68c6097c1fd6135c9f429d94f9d05cd9
Neo <teoh1993@hotmail.com>
2019-09-04 21:09:38 +0800

    update env

---
+++ tutorial/index.js
@@ -0,0 +1,20 @@
+
+
+console.log('hello world');
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+
+
+console.log('hello world');
+
+
+
+
+
+
+
+}).listen(8080);
