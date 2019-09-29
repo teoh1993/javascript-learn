3b04aea91afe9f715e95f52c2cb8615ff9815ca3
Neo <teoh1993@hotmail.com>
2019-09-29 19:12:16 +0800

    update express js

--- js_fundamental/app.js
+++ js_fundamental/app.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  console.log('1xxx1zz');
+
+  res.send('Hello World');
 })
 
-app.listen(3000)
+app.listen(3000);
